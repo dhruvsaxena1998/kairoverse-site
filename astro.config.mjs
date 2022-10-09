cc1e58eff60e4a55d6add61840f1c9517e1ba7e4
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-10-09 14:51:45 +0530

    chore: added site url to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kairoverse.com",
   integrations: [
     tailwind(),
     mdx(),
