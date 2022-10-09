5cd72d7755327c46d50e9e1135a16e19a2c91950
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-10-09 13:59:57 +0530

    feat: added basic analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import preact from "@astrojs/preact";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), preact()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    preact(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
