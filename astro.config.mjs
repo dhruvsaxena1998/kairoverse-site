1dfcb948f038ae41794813e33cb9b1a8f849f7ac
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2022-10-11 22:29:26 +0530

    ci: added sitemap generator and new favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,21 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kairoverse.com",
-  integrations: [
-    tailwind(),
-    mdx(),
-    preact(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  site: 'https://kairoverse.com',
+  integrations: [tailwind(), mdx(), preact(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap({
+    customPages: [
+      'https://kairoverse.com',
+      'https://kairoverse.com/about'
+    ]
+  })],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
